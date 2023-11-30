caefefb395b748864dbc559c16b19c0a85d79aea
standardforever <standard.forever123@gmail.cm>
2023-11-30 09:33:19 +0100

    update

--- controllers/post.js
+++ controllers/post.js
@@ -5,26 +5,30 @@ import mongoose from "mongoose";
 
 
 const makePost = asyncHandler(async (req, res) => {
-    const { desc, photo, location, friendTags, sharewithgroups, sharewithpages } = req.body;
-    const userId = mongoose.Types.ObjectId(req.params.userId);
-  
-    // Create an object with only the fields that are present in the request body
-    const postFields = {
-      desc,
-      photo,
-      postedBy: userId,
-      location,
-    };
-  
-    // Add optional fields if they are present
-    if (friendTags) postFields.friendTags = friendTags;
-    if (sharewithgroups) postFields.sharewithgroups = sharewithgroups;
-    if (sharewithpages) postFields.sharewithpages = sharewithpages;
-  
-    const newPost = new Post(postFields);
-  
-    const savedPost = await newPost.save();
-    res.status(201).json(savedPost);
+  try{
+      const { desc, photo, location, friendTags, sharewithgroups, sharewithpages } = req.body;
+      const userId = mongoose.Types.ObjectId(req.params.userId);
+    
+      // Create an object with only the fields that are present in the request body
+      const postFields = {
+        desc,
+        photo,
+        postedBy: userId,
+        location,
+      };
+    
+      // Add optional fields if they are present
+      if (friendTags) postFields.friendTags = friendTags;
+      if (sharewithgroups) postFields.sharewithgroups = sharewithgroups;
+      if (sharewithpages) postFields.sharewithpages = sharewithpages;
+    
+      const newPost = new Post(postFields);
+    
+      const savedPost = await newPost.save();
+      return res.status(201).json(savedPost);
+    } catch(error) {
+      return res.status(500).json({error: error})
+    }
   });
   
 
@@ -33,43 +37,57 @@ const makePost = asyncHandler(async (req, res) => {
 // @route   GET /api/posts
 // @access  Private
 const getPostsForUser = asyncHandler(async (req, res) => {
+  try{
     const userId = mongoose.Types.ObjectId(req.params.user_id);
     const posts = await Post.find({ postedBy: userId }).populate('postedBy', 'username');
   
-    res.status(200).json(posts);
+    return res.status(200).json(posts);
+  } catch(error) {
+    return res.status(500).json({error: error})
+  }
   });
   
   // @desc    Get a specific post for a user
   // @route   GET /api/posts/:postId
   // @access  Private
   const getPostForUser = asyncHandler(async (req, res) => {
+    try{
     // const userId = mongoose.Types.ObjectId(req.params.user_id);
     const postId = mongoose.Types.ObjectId(req.params.post_id);
   
     const post = await Post.findOne({ _id: postId }).populate('postedBy', 'username');
   
     if (!post) {
-      res.status(404).json({ error: 'Post not found' });
+      return res.status(404).json({ error: 'Post not found' });
     } else {
-      res.status(200).json(post);
+     return  res.status(200).json(post);
     }
+
+  } catch(error) {
+    return res.status(500).json({error: error})
+  }
   });
   
   // @desc    Delete a specific post for a user
   // @route   DELETE /api/posts/:postId
   // @access  Private
   const deletePostForUser = asyncHandler(async (req, res) => {
+    try{
     const userId = req.user.id;
     const postId = req.params.postId;
   
     const post = await Post.findOne({ _id: postId, postedBy: userId });
   
     if (!post) {
-      res.status(404).json({ error: 'Post not found' });
+      return res.status(404).json({ error: 'Post not found' });
     } else {
       await post.remove();
-      res.status(204).json({ success: 'Post deleted successfully' });
+     return res.status(204).json({ success: 'Post deleted successfully' });
     }
+
+  } catch(error) {
+    return res.status(500).json({error: error})
+  }
   });
   
 
@@ -94,11 +112,11 @@ const getPostsForUser = asyncHandler(async (req, res) => {
         }
   
         const updatedPost = await post.save();
-        res.status(200).json(updatedPost);
+        return res.status(200).json(updatedPost);
       }
     } catch (error) {
       console.error('Error toggling like:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
   });
   
@@ -132,7 +150,7 @@ const getPostsForUser = asyncHandler(async (req, res) => {
       res.status(200).json(updatedPost);
     } catch (error) {
       console.error("Error adding comment:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+      return res.status(500).json({ error: "Internal Server Error" });
     }
   });
     
