b3eeb8217001adc67498d868ba87b0e035310f9e
Dev sam <lemikan2000@gmail.com>
2024-04-03 10:44:31 +0100

    addedd authentication to cloud endpoints

--- router/cloud_route.js
+++ router/cloud_route.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import { createCloud, getCloudUser, upgradeCloud } from '../controllers/cloudcontroller.js';
 const cloudRouter= express.Router();
+import { authenticate } from '../middleWare/auth.js';
 
-cloudRouter.post('/create-cloud', createCloud)
-cloudRouter.get('/get-cloud-user/:user_id', getCloudUser)
-cloudRouter.post('/upgrade-cloud/:user_id', upgradeCloud)
+
+cloudRouter.post('/create-cloud',authenticate, createCloud)
+cloudRouter.get('/get-cloud-user/:user_id',authenticate, getCloudUser)
+cloudRouter.post('/upgrade-cloud/:user_id',authenticate, upgradeCloud)
 
 
 export default cloudRouter;
